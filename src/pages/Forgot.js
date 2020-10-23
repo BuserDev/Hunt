ef57fe6d04b13c938bb471c71eca8e406cb97320
Robert Buser <rob@RongBip.local>
2020-10-23 09:33:00 -0700

    Forgot Page and Validation

--- src/pages/Forgot.js
+++ src/pages/Forgot.js
@@ -10,29 +10,56 @@ import {
     IonButton,
     IonLoading,
 } from "@ionic/react";
-// import firebase from "../firebase";
-// import { toast } from "../utils/toast";
-// import useFormValidation from "../hooks/useFormValidation";
-// import validatePasswordReset from "../components/Auth/validatePasswordReset";
+import firebase from "../firebase";
+import { toast } from "../utils/toast";
+import useFormValidation from "../hooks/useFormValidation";
+import validatePasswordReset from "../components/Auth/validatePasswordReset";
 import NavHeader from "../components/Header/NavHeader";
 
-// const INITIAL_STATE = {
-//     email: "",
-// };
+const INITIAL_STATE = {
+    email: "",
+};
 
 const Forgot = (props) => {
+    const {
+        handleSubmit,
+        handleChange,
+        values,
+        isSubmitting,
+    } = useFormValidation(
+        INITIAL_STATE,
+        validatePasswordReset,
+        handleResetPassword
+    );
+
+    const [busy, setBusy] = React.useState(false);
+
+    async function handleResetPassword() {
+        setBusy(true);
+        const { email } = values;
+        try {
+            await firebase.resetPassword(email);
+            toast("Check your email to reset your password.");
+            props.history.push("/login");
+        } catch (err) {
+            console.error("Password Reset Error", err);
+            toast(err.message);
+        }
+        setBusy(false);
+    }
+
     return (
         <IonPage>
             <NavHeader title="Password Reset" />
-            {/* <IonLoading message={"Please wait..."} isOpen={busy} /> */}
+            <IonLoading message={"Please wait..."} isOpen={busy} />
             <IonContent>
                 <IonItem lines="full">
                     <IonLabel position="floating">Email</IonLabel>
                     <IonInput
                         name="email"
-                        // value={values.email}
+                        value={values.email}
                         type="text"
-                        // onIonChange={handleChange}
+                        onIonChange={handleChange}
                         required
                     ></IonInput>
                 </IonItem>
@@ -42,8 +69,8 @@ const Forgot = (props) => {
                             type="submit"
                             color="primary"
                             expand="block"
-                        //   onClick={handleSubmit}
-                        //   disabled={isSubmitting}
+                            onClick={handleSubmit}
+                            disabled={isSubmitting}
                         >
                             Get Reset Link
             </IonButton>
